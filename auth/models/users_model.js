3b31a874c52cb7c8b0f5cc944851855e08b3ee9b
ShinjinE <shinjinethan@hotmail.com>
2018-12-11 22:52:37 +0000

    users

--- auth/models/users_model.js
+++ auth/models/users_model.js
@@ -3,7 +3,16 @@ var mongoose = require('mongoose'),
 var UserSchema = new Schema({
     username: { type: String, unique: true },
     email: String,
-    color: String,
+    B1: String,
+    B2: String,
+    B3: String,
+    B4: String,
+    B5: String,
+    B6: String,
+    B7: String,
+    B8: String,
+    B9: String,
+    B10: String,
     hashed_password: String
 });
 mongoose.model('User', UserSchema);
